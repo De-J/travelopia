0a1d515c43d1f75fa81256e782313da301752646
Dev Joshi <devjtitan@gmail.com>
2024-02-10 18:29:57 +0530

    add overlay toggle functionality

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const alertBtn = document.getElementById("alert");
 alertBtn.addEventListener("click", () => {
     alert("Hello, World")
 });
+
+const overlayBtn = document.getElementById("toggle");
+const container = document.getElementById("container");
+overlayBtn.addEventListener("click", () => {
+  container.classList.toggle("color");
+});
